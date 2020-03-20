4ae71a6ec6d371009d0ae15208024e639070e483
Jahongir2003 <55879642+Jahongir2003@users.noreply.github.com>
2020-03-20 22:49:33 +0500

    Update

--- plugins/upload.py
+++ plugins/upload.py
@@ -82,7 +82,7 @@ async def progress(cur, tot, start_time, status, snt):
 
             progress = round((cur * 100) / tot,2)
 
-            text = f"**{status}**\n\n`{progress}%` done.\n**{curr}MB** of **{tott}MB**\nSpeed: **{speed}MBPS**\nETA: **{eta}**"
+            text = f"**{status}**\n\n`{progress}%` Bajarildi✔.\n**{tott}MB** dan **{curr}MB**\n⏱Tezlik: **{speed}MB/S**\n⌚Qolgan Vaqt: **{eta}**"
 
             await snt.edit_text(text = text)
 
